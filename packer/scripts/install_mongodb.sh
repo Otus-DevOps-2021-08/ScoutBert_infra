11f8fb1a507c282379f7879f7acc58c222e3cba5
Albert Sabitov <scout.berty@gmail.com>
2021-09-30 16:07:00 +0300

    fix scripts

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
-wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-apt-get update
-apt install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
+#!/bin/bash
+#upload repo
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#upd repo
+sudo apt-get update
+rm /var/lib/apt/lists/lock
+rm /var/cache/apt/archives/lock
+rm /var/lib/dpkg/lock
+rm /var/lib/dpkg/lock-frontend
+dpkg --configure -a
+#install mongodb
+sudo apt-get install -y mongodb-org
+#start mongodb
+sudo systemctl start mongod
+#add autorun
+sudo systemctl enable mongod
--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-apt update
-apt install -y ruby-full ruby-bundler build-essential
+#!/bin/bash
+#update OS
+apt-get update
+rm /var/lib/apt/lists/lock
+rm /var/cache/apt/archives/lock
+rm /var/lib/dpkg/lock
+rm /var/lib/dpkg/lock-frontend
+dpkg --configure -a
+#install ruby and bundler
+apt-get install -y ruby-full ruby-bundler build-essential
