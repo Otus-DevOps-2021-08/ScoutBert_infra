c1b5f98c5a3034c407b339e73ff8948dfc5092f3
Albert Sabitov <scout.berty@gmail.com>
2021-10-05 17:56:53 +0300

    fix

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 APP_DIR=${1:-$HOME}
 sudo apt-get install -y git
-git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+git clone -b monolith https://github.com/express42/reddit.git
 cd $APP_DIR/reddit
 bundle install
 sudo mv /tmp/puma.service /etc/systemd/system/puma.service
