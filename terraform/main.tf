3ccf92cadb72d5aa55e30f014e8a9cbf35fd69f1
Albert Sabitov <scout.berty@gmail.com>
2021-10-05 18:28:19 +0300

    fix main

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,10 @@ provider "yandex" {
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
 
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/ubuntu.pub")}"
+  }
+
   resources {
     cores  = 2
     memory = 2
@@ -22,36 +26,29 @@ resource "yandex_compute_instance" "app" {
 
   boot_disk {
     initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
       image_id = "fd80dmou2coj1e56hos5"
     }
   }
 
   network_interface {
-    # Указан id подсети default-ru-central1-a
     subnet_id = "e9bvegsa6vvdst24ie0m"
     nat       = true
   }
 
-  metadata = {
-  ssh-keys = "ubuntu:${file("~/.ssh/ubuntu.pub")}"
-  }
-
   connection {
-  type = "ssh"
-  host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-  user = "ubuntu"
-  agent = false
-  # путь до приватного ключа
-  private_key = file("~/.ssh/ubuntu")
+    type        = "ssh"
+    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file("~/.ssh/ubuntu")
   }
 
   provisioner "file" {
-  source = "files/puma.service"
-  destination = "/tmp/puma.service"
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
   }
 
   provisioner "remote-exec" {
-  script = "files/deploy.sh"
+    script = "files/deploy.sh"
   }
 }
