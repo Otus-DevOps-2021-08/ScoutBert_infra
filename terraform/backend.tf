5247ae75d4c29ce11ff4c5a0092d6b5be0466037
Albert Sabitov <scout.berty@gmail.com>
2021-10-11 23:09:30 +0300

    add backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    yandex = "0.35"
+  }
+
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "terraform-object-storage-tutorial" //var.yandex_backed_name
+    region   = "ru-central1-a"
+    key      = "terraform.tfstate"
+  }
+}
